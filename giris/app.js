674404192f08387a58ed82c550de471e21004d66
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-18 16:03:24 +0300

    graphql installation

--- giris/app.js
+++ giris/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+
+}));
 
 app.listen(3000, () => {
 	console.log('Server is running...');
