496355d048344208dad0e34301e8da813a34911a
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-19 12:03:05 +0300

    graphql installation

--- movie-app/app.js
+++ movie-app/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+
+}));
 
 app.listen(5000, () => {
 	console.log('server is running...');
