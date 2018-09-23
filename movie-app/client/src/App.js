ad9c9ee0bb44d93df9d7dd698ae6fb41ad46ee3d
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-23 13:25:07 +0300

    apollo client installation

--- movie-app/client/src/App.js
+++ movie-app/client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+	url: 'http://localhost:5000/graphql'
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>my app</p>
-      </div>
+			<ApolloProvider client={client}>
+				<div className="App">
+					<p>my app</p>
+				</div>
+			</ApolloProvider>
     );
   }
 }
