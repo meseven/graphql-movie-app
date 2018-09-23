8336e49163e9e18f97d12961491ac623b3ef026c
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-23 14:48:22 +0300

    Query component

--- movie-app/client/src/components/MovieList.js
+++ movie-app/client/src/components/MovieList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import { gql } from 'apollo-boost';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 const getMoviesQuery = gql`
 	{
@@ -14,27 +14,26 @@ const getMoviesQuery = gql`
 `;
 
 class MovieList extends Component {
-	listMovies(){
-		const { data } = this.props;
-
-		if (data.loading)
-		  return <div>Loading...</div>;
-		else
-			return data.movies.map(movie => <li key={movie.id}>{movie.title}</li>)
-	}
-
 	render() {
-		console.log(this.props);
 		return (
 			<div>
 				<ul className="movie-list">
-					{
-						this.listMovies()
-					}
+					<Query query={getMoviesQuery}>
+						{({ loading, error, data }) => {
+							if (loading) return <div>Loading...</div>;
+							if (error) return <div>Error.</div>;
+
+							return data.movies.map(({ id, title }) => (
+								<li key={id}>
+									{title}
+								</li>
+							))
+						}}
+					</Query>
 				</ul>
 			</div>
 		);
 	}
 }
 
-export default graphql(getMoviesQuery)(MovieList);
+export default MovieList;
