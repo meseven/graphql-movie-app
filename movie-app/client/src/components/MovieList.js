1a40c87f030de77a5fa4cabdf2fc1b80170f6b9a
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-30 17:17:49 +0300

    css bug fix

--- movie-app/client/src/components/MovieList.js
+++ movie-app/client/src/components/MovieList.js
@@ -4,10 +4,51 @@ import { Query } from 'react-apollo';
 // queries
 import { getMoviesQuery } from '../queries/queries';
 
+import { Modal } from 'antd';
+
+
 class MovieList extends Component {
+
+	state = { visible: false };
+
+
+	showModal = () => {
+		this.setState({
+			visible: true,
+		});
+	}
+
+	handleOk = (e) => {
+		console.log(e);
+		this.setState({
+			visible: false,
+		});
+	}
+
+	handleCancel = (e) => {
+		console.log(e);
+		this.setState({
+			visible: false,
+		});
+	}
+
+
 	render() {
 		return (
 			<div className="container" data-state="Movie App">
+
+				<Modal
+					centered
+					title="Basic Modal"
+					visible={this.state.visible}
+					onOk={this.handleOk}
+					onCancel={this.handleCancel}
+				>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+				</Modal>
+
 				<div className="device" data-view="list">
 					<ul className="movie-list layer" data-layer="list">
 						<Query query={getMoviesQuery}>
@@ -16,7 +57,7 @@ class MovieList extends Component {
 								if (error) return <div>Error.</div>;
 
 								return data.movies.map(({ id, title, description }) => (
-									<li className="content" key={id}>
+									<li className="content" key={id} onClick={this.showModal}>
 										<div className="bg"></div>
 										<div className="avatar"></div>
 										<div className="title">{title}</div>
