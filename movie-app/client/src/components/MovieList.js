7abeb1ffb561d4c2490eb76cd9afa8895ce69455
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-01 11:42:43 +0300

    antd modal

--- movie-app/client/src/components/MovieList.js
+++ movie-app/client/src/components/MovieList.js
@@ -4,9 +4,51 @@ import { Query } from 'react-apollo';
 // queries
 import { getMoviesQuery } from '../queries/queries';
 
-class MovieList extends Component {render() {
+import { Button, Modal } from 'antd';
+import 'antd/dist/antd.css';
+
+class MovieList extends Component {
+	state = {
+		visible: false
+	};
+
+	showModal = id => {
+		console.log(id);
+		this.setState({
+			visible: true,
+		});
+	};
+
+	handleOk = (e) => {
+		this.setState({
+			visible: false,
+		});
+	};
+
+	handleCancel = (e) => {
+		this.setState({
+			visible: false,
+		});
+	};
+
+	render() {
 		return (
 			<div className="container" data-state="Movie App">
+				<Modal
+					title="Basic Modal"
+					visible={this.state.visible}
+					onOk={this.handleOk}
+					onCancel={this.handleCancel}
+					footer={[
+						<Button key="submit" type="primary" onClick={this.handleOk}>
+							OK
+						</Button>,
+					]}
+				>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+				</Modal>
 				<div className="device" data-view="list">
 					<ul className="movie-list layer" data-layer="list">
 						<Query query={getMoviesQuery}>
@@ -15,7 +57,9 @@ class MovieList extends Component {render() {
 								if (error) return <div>Error.</div>;
 
 								return data.movies.map(({ id, title, description }) => (
-									<li className="content" key={id} onClick={this.showModal}>
+									<li className="content" key={id} onClick={() => {
+										this.showModal(id)
+									}}>
 										<div className="bg"></div>
 										<div className="avatar"></div>
 										<div className="title">{title}</div>
