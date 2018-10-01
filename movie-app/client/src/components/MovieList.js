b2547971383eca8c7053a72b4507a9edb142732b
Mehmet Seven <mehmetseven0@gmail.com>
2018-10-01 11:11:51 +0300

    test

--- movie-app/client/src/components/MovieList.js
+++ movie-app/client/src/components/MovieList.js
@@ -4,51 +4,9 @@ import { Query } from 'react-apollo';
 // queries
 import { getMoviesQuery } from '../queries/queries';
 
-import { Modal } from 'antd';
-
-
-class MovieList extends Component {
-
-	state = { visible: false };
-
-
-	showModal = () => {
-		this.setState({
-			visible: true,
-		});
-	}
-
-	handleOk = (e) => {
-		console.log(e);
-		this.setState({
-			visible: false,
-		});
-	}
-
-	handleCancel = (e) => {
-		console.log(e);
-		this.setState({
-			visible: false,
-		});
-	}
-
-
-	render() {
+class MovieList extends Component {render() {
 		return (
 			<div className="container" data-state="Movie App">
-
-				<Modal
-					centered
-					title="Basic Modal"
-					visible={this.state.visible}
-					onOk={this.handleOk}
-					onCancel={this.handleCancel}
-				>
-					<p>Some contents...</p>
-					<p>Some contents...</p>
-					<p>Some contents...</p>
-				</Modal>
-
 				<div className="device" data-view="list">
 					<ul className="movie-list layer" data-layer="list">
 						<Query query={getMoviesQuery}>
