a8b1217a11d2a81fd0309f6fc3fc4600118e8f4b
Mehmet Seven <mehmetseven0@gmail.com>
2020-02-21 19:22:35 +0300

    graphql endpoint changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 // components
-import MovieList from './components/MovieList';
-import NewMovieForm from './components/NewMovieForm';
+import MovieList from "./components/MovieList";
+import NewMovieForm from "./components/NewMovieForm";
 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-	uri: 'http://localhost:5000/graphql'
+	uri: "http://67.205.135.230:5000/graphql"
 });
 
 class App extends Component {
-  render() {
-    return (
+	render() {
+		return (
 			<ApolloProvider client={client}>
 				<div className="App">
-					<NewMovieForm/>
+					<NewMovieForm />
 					<MovieList />
 				</div>
 			</ApolloProvider>
-    );
-  }
+		);
+	}
 }
 
 export default App;
